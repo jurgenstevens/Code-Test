a465023db3a02791afd595d0062de1e11deffe54
Jurgen <jurgen.stevens@yahoo.com>
2020-07-09 18:17:56 -0500

    Returns json renders on the page.

--- code-test/src/App.js
+++ code-test/src/App.js
@@ -1,14 +1,34 @@
-import React from "react";
-import "./App.css";
+import React, { Component } from "react";
+// installed semantic-ui-react
+import { Container, Grid, Header, List } from "semantic-ui-react";
 // let's start off by importing the json
 import returns from "./returns/returns.json";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello from App.js</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Container>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column>
+              <Header>Year | Return</Header>
+              <List>
+                {returns.map((r) => {
+                  return (
+                    <List.Item key={r.id}>
+                      <List.Content>
+                        {r.year} | {r.totalReturn}
+                      </List.Content>
+                    </List.Item>
+                  );
+                })}
+              </List>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
+    );
+  }
 }
 
 export default App;
