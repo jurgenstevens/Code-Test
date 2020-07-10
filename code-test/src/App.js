7703344f3293e4d9f2b318eaa260e65a043b9d3f
Jurgen <jurgen.stevens@yahoo.com>
2020-07-09 23:26:35 -0500

    Installed semantic-ui-css and added the header to App.js

--- code-test/src/App.js
+++ code-test/src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
 // installed semantic-ui-react
 import { Container, Grid, Header, List } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
 // let's start off by importing the json
 import returns from "./returns/returns.json";
+// import the header from the components folder
+import HeaderComponent from "./components/Header";
 
 // map the returns json and use the reverse method for the ascending order requested
 class App extends Component {
   render() {
     return (
       <Container>
+        <HeaderComponent />
         <Grid>
           <Grid.Row>
             <Grid.Column>
