4d5b4d488dd597a338372cd8dcc3c8e035b0adfc
Jurgen <jurgen.stevens@yahoo.com>
2020-07-09 23:12:13 -0500

    Simple header created.

---
+++ code-test/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Header } from "semantic-ui-react";
+// make a simple header
+const Header = () => (
+  <div>
+    <Header size="huge">RetireUp Code Test</Header>
+    <Header size="medium">S&P 500 Index</Header>
+  </div>
+);
+
+export default Header;
