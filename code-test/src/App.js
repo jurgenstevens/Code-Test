ab08d66cbd32f9aa28e82c5654abd10315a85ec0
Jurgen <jurgen.stevens@yahoo.com>
2020-07-13 00:02:56 -0500

    state set up to be export props for slider and table components.

--- code-test/src/App.js
+++ code-test/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 // installed semantic-ui-react
 import { Container } from "semantic-ui-react";
+// import the returns json
+import returns from "./returns/returns.json";
 import "semantic-ui-css/semantic.min.css";
 import Slider from "./components/Slider";
 import Table from "./components/Table";
@@ -8,15 +10,31 @@ import "rc-slider/assets/index.css";
 
 class App extends Component {
   // we will use state so the values can change
+  constructor(props) {
+    // this will sort the json's years by ascending
+    let sortedReturns = returns.sort((a, b) => a.year - b.year);
+    // the slider changes as you add/remove data
+    let min = sortedReturns[0]["year"];
+    let max = sortedReturns[returns.length - 1]["year"];
+
+    // the props will be exported for the slider and table components functionality
+    super(props);
+    this.state = {
+      returns: sortedReturns,
+      min: min,
+      max: max,
+      sliderValues: [min, max],
+    };
+  }
+
   render() {
     return (
-      <div>
-        <h1>Hello from App.js</h1>
+      <Container>
         {/* Below is the Slider component */}
         <Slider />
         {/* Below is the Table component */}
         <Table />
-      </div>
+      </Container>
     );
   }
 }
